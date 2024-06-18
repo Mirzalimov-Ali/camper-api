846473cb9658ca367ed05cee9e48118b3898b3ef
unknown <alidev1414@gmail.com>
2024-06-18 18:46:43 +0900

    Add: password hash

---
+++ utils/errorResponse.js
@@ -0,0 +1,8 @@
+class ErrorRespoonse extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+module.exports = ErrorRespoonse
